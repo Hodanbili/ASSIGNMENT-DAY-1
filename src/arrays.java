88a48f5babaf3e0ce9ec347ecf74200405c87c6e
Hodan Isse <hodanissah3@gmail.com>
2022-11-13 03:39:43 -0800

    first commit

---
+++ src/arrays.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class arrays {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        int sum=a+b;
+
+            System.out.println(sum);
+
+        int year = 2000;
+// if a year is multiple of 400;
+// then it is a Leap year
+        If ("year % 400 ==0");
+        System.out.println("Leap year");
+        // else if a year is multiple of 100,
+        // then it is not a leap year
+        If ("year % 100 == 0");
+        System.out.println("Leap year");
+// if a year is multiple of 4,
+// then it is a leap year
+        If ("year % 4 ==0");
+        System.out.println("Leap year");
+
+    }
+
+    private static void If(String s) {
+
+
+    }
+}
